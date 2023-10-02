2f4051a8d224b5d934f909453debe4a9a55fbbdf
Jardel Galvão <jardelgalvao1@gmail.com>
2023-10-01 22:12:44 -0300

    Reduzimos código em serializer

--- agenda/models.py
+++ agenda/models.py
@@ -6,3 +6,4 @@ class Agendamento(models.Model):
     nome_cliente = models.CharField(max_length=256)
     email_cliente = models.EmailField()
     telefone_cliente = models.CharField(max_length=256)
+    cancelado = models.BooleanField(default=False)
--- agenda/serializers.py
+++ agenda/serializers.py
@@ -1,7 +1,22 @@
 from rest_framework import serializers
+from agenda.models import Agendamento
+from django.utils import timezone
 
-class AgendamentoSerializer(serializers.Serializer):
-    data_horario = serializers.DateTimeField()
-    nome_cliente = serializers.CharField(max_length = 256)
-    email_cliente = serializers.EmailField()
-    telefone_cliente = serializers.CharField(max_length=256)
+class AgendamentoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Agendamento
+        fields = ["id", "data_horario", "nome_cliente", "email_cliente", "telefone_cliente"]
+
+    def validate_data_horario(self, value):
+        if value < timezone.now():
+            raise serializers.ValidationError("Agendamento não pode ser feito no passado!")
+        return value
+    
+    def validate(self, attrs):
+        
+        email_cliente = attrs.get("email_cliente", "")
+        telefone_cliente = attrs.get("telefone_cliente", "")
+
+        if email_cliente.endswith(".br") and telefone_cliente.startswith("+") and not telefone_cliente.startswith("+55"):
+            raise serializers.ValidationError("Email brasileito deve estar associado a um número do Brasil (+55)")
+        return attrs
