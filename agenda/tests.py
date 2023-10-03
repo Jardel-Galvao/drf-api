81709fa04df4764e54b2cc2c4c6570751a8f7972
Jardel Galvão <jardelgalvao1@gmail.com>
2023-10-03 16:16:56 -0300

    Criados generics views

--- agenda/tests.py
+++ agenda/tests.py
@@ -1,2 +1,119 @@
 from rest_framework.test import APITestCase
+import json
+from datetime import datetime, timezone
+from agenda.models import Agendamento
 
+class TestListagemAgendamentos(APITestCase):
+    def test_listagem_vazia(self):
+        response = self.client.get("/api/agendamentos/")
+        data = json.loads(response.content)
+        self.assertEqual(data, [])
+    
+    def test_listagem_de_agendamentos_criados(self):
+        Agendamento.objects.create(
+            data_horario=datetime(2099,3,15,17,30, tzinfo=timezone.utc),
+            nome_cliente = "Teste",
+            email_cliente = "teste@teste.com",
+            telefone_cliente = "4899999999",
+        )
+
+        Agendamento_serializado = {
+            "id" : 1,
+            "data_horario": "2099-03-15T17:30:00Z",
+            "nome_cliente" : "Teste",
+            "email_cliente" : "teste@teste.com",
+            "telefone_cliente" : "4899999999",
+        }
+
+        response = self.client.get("/api/agendamentos/")
+        data = json.loads(response.content)
+        self.assertDictEqual(data[0], Agendamento_serializado)
+    
+class TestCriacaoAgendamento(APITestCase):
+    def test_cria_agendamento(self):
+        data =  {
+            "data_horario": datetime(2099, 5, 1),
+            "nome_cliente" : "Teste",
+            "email_cliente" : "teste@teste.com",
+            "telefone_cliente" : "4899999999",
+        }
+
+        Agendamento_serializado = {
+            "id" : 1,
+            "data_horario": "2099-05-01T00:00:00Z",
+            "nome_cliente" : "Teste",
+            "email_cliente" : "teste@teste.com",
+            "telefone_cliente" : "4899999999",
+        }
+
+        response_post = self.client.post("/api/agendamentos/", data, format='json')
+        response_get = self.client.get("/api/agendamentos/")
+        data = json.loads(response_get.content)
+
+        self.assertEqual(data[0], Agendamento_serializado)
+    
+    def test_quando_request_e_invalido_retorna_400(self):
+        data = {
+            "nome_cliente" : "Teste",
+            "email_cliente" : "teste@teste.com",
+            "telefone_cliente" : "4899999999",
+        }
+        response = self.client.post("/api/agendamentos/", data, format='json')
+        self.assertEqual(response.status_code, 400)
+
+class TestDetalharAgendamento(APITestCase):
+    def test_detalhar_agendamento(self):
+        data = {
+                "data_horario": datetime(2099, 5, 1),
+                "nome_cliente" : "Teste",
+                "email_cliente" : "teste@teste.com",
+                "telefone_cliente" : "4899999999",
+        }
+
+        Agendamento_serializado = {
+            "id" : 1,
+            "data_horario": "2099-05-01T00:00:00Z",
+            "nome_cliente" : "Teste",
+            "email_cliente" : "teste@teste.com",
+            "telefone_cliente" : "4899999999",
+        }
+        response_post = self.client.post("/api/agendamentos/", data, format='json')
+        response_get = self.client.get("/api/agendamentos/1/")
+        data = json.loads(response_get.content)
+        self.assertDictEqual(data, Agendamento_serializado)
+
+    def test_editar_agendamento(self):
+        data = {
+               "data_horario": datetime(2099, 5, 1),
+               "nome_cliente" : "Teste",
+               "email_cliente" : "teste@teste.com",
+               "telefone_cliente" : "4899999999",
+        }
+        edicao_agendamento = {"nome_cliente" : "Teste2"}
+        Agendamento_serializado = {
+            "id" : 1,
+            "data_horario": "2099-05-01T00:00:00Z",
+            "nome_cliente" : "Teste2",
+            "email_cliente" : "teste@teste.com",
+            "telefone_cliente" : "4899999999",
+        }
+        response_post = self.client.post("/api/agendamentos/", data, format='json')
+        response_patch = self.client.patch("/api/agendamentos/1/", edicao_agendamento, format='json')
+        response_get = self.client.get("/api/agendamentos/1/")
+        data = json.loads(response_get.content)
+        self.assertDictEqual(data, Agendamento_serializado)
+    
+    def test_cancelar_agendamento(self):
+        data = {
+            "data_horario": datetime(2099, 5, 1),
+            "nome_cliente" : "Teste",
+            "email_cliente" : "teste@teste.com",
+            "telefone_cliente" : "4899999999",
+        }
+        response_post = self.client.post("/api/agendamentos/", data, format='json')
+        response_delete = self.client.patch("/api/agendamentos/1/")
+        obj = Agendamento.objects.get()
+        obj.cancelado = True
+        obj.save()
+
+        self.assertEqual(obj.cancelado, True)
--- agenda/views.py
+++ agenda/views.py
@@ -4,39 +4,12 @@ from agenda.models import Agendamento
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
-@api_view(http_method_names=["GET", "PATCH", "DELETE"])
-def agendamento_detail(request, id):
-    obj = get_object_or_404(Agendamento, id=id)
-    if request.method == "GET":
-        serializer = AgendamentoSerializer(obj)
-        return JsonResponse(serializer.data)
-    
-    if request.method == "PATCH":
-        serializer = AgendamentoSerializer(obj, data=request.data, partial=True)
-        if serializer.is_valid():
-            v_data = serializer.validated_data
-            serializer.save()
-            return JsonResponse(v_data, status=200)
-        return JsonResponse(serializer.errors, status=400)
-    
-    if request.method == "DELETE":
-        obj.cancelado = True
-        obj.save()
-        return Response(status=204)
-
-@api_view(http_method_names=["GET", "POST"])
-def agendamento_list(request):
-    if request.method == "GET":
-        qs = Agendamento.objects.filter(cancelado=False)
-        serializer = AgendamentoSerializer(qs, many=True)
-        return JsonResponse(serializer.data, safe=False)
-    
-    if request.method == "POST":
-        data = request.data
-        serializer = AgendamentoSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        
-    return JsonResponse(serializer.errors, status=400)
+from rest_framework.views import APIView
+from rest_framework import mixins, generics
+from datetime import datetime
+class AgendamentoList(generics.ListCreateAPIView):
+    queryset = Agendamento.objects.all()
+    serializer_class = AgendamentoSerializer
+class AgendamentoDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Agendamento.objects.all()
+    serializer_class = AgendamentoSerializer
